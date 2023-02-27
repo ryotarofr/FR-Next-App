3e3d189aa32dc95a7d712c68a0fff379503decc2
ryotarofr <ryoryo.fr0608@icloud.com>
2023-02-27 12:56:09 +0900

    ➕ login form

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,12 @@
-
 import LoginButton from '../components/Login';
 import { useState } from 'react'
 import styles from "../styles/Login.module.css"
 import { useForm, SubmitHandler } from 'react-hook-form'
+import { AiOutlineUser } from "react-icons/ai"
+import { RiLockPasswordLine } from "react-icons/ri"
 import useAuth from '../hooks/useAuth'
-import Box from "@mui/material/Box";
+
+
 
 interface Inputs {
   email: string
@@ -12,7 +14,6 @@ interface Inputs {
 }
 
 const LoginPage = () => {
-  const path = "/assets/bg1.png";
   const [login, setLogin] = useState(true)
   const { signIn, signUp } = useAuth()
 
@@ -37,70 +38,64 @@ const LoginPage = () => {
 
   return (<>
     <div className={styles.login}>
-      <Box
-        sx={{
-          backgroundImage: `url(${path})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          overflow: "hidden",
-          height: "100vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
 
-        <form onSubmit={handleSubmit(onSubmit)}>
-          {toggle ? (<h1 className={styles.h1}>Sign Up</h1>) : (<h1 className={styles.h1}>Sign In</h1>)}
+      <div className={styles.box}>
+        <div className={styles.container}>
 
-          <div className={styles.inputWrapper}>
-            <label className={styles.label}>
-              <input
-                className={styles.input}
-                type="email"
-                placeholder="Email"
-                {...register('email', { required: true })}
-              />
-              {errors.email && (
-                <p className={styles.inputError}>
-                  正しいメールアドレスを入力してください。
-                </p>
-              )}
-            </label>
-            <label className={styles.label}>
-              <input
-                className={styles.input}
-                type="password"
-                placeholder="password"
-                {...register('password', { required: true })}
-              />
-              {errors.password && (
-                <p className={styles.inputError}>
-                  パスワードは 6 ～ 60 文字である必要があります。
-                </p>
-              )}
-            </label>
-            {toggle ?
-              <>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            {toggle ? (<h1 className={styles.h1}>Sign Up</h1>) : (<h1 className={styles.h1}>Sign In</h1>)}
 
-                <button className={styles.button} onClick={() => setLogin(false)}>signup</button>
-                <div className={styles.div1}>すでにアカウントをお持ちの方:<a className={styles.a} onClick={toggleBool}>signin</a></div>
-              </>
-              : <>
+            <div className={styles.inputWrapper}>
+              <label className={styles.label}>
+                <AiOutlineUser className={styles.bx} />
+                <input
+                  className={styles.input}
+                  type="email"
+                  placeholder="Email"
+                  {...register('email', { required: true })}
+                />
 
-                <button className={styles.button} onClick={() => setLogin(true)}>signin</button>
-                <div className={styles.div1}>アカウントをお持ちでない方:<a className={styles.a} onClick={toggleBool}>signup now</a></div>
-              </>}
+                {errors.email && (
+                  <p className={styles.inputError}>
+                    正しいメールアドレスを入力してください。
+                  </p>
+                )}
+              </label>
+              <label className={styles.label}>
+                <RiLockPasswordLine className={styles.bx} />
+                <input
+                  className={styles.input}
+                  type="password"
+                  placeholder="password"
+                  {...register('password', { required: true })}
+                />
+
+                {errors.password && (
+                  <p className={styles.inputError}>
+                    パスワードは 6 ～ 60 文字である必要があります。
+                  </p>
+                )}
+              </label>
+              {toggle ?
+                <>
+
+                  <button className={styles.button} onClick={() => setLogin(false)}>signup</button>
+                  <div className={styles.div1}>すでにアカウントをお持ちの方:<a className={styles.a} onClick={toggleBool}>signin</a></div>
+                </>
+                : <>
+
+                  <button className={styles.button} onClick={() => setLogin(true)}>signin</button>
+                  <div className={styles.div1}>アカウントをお持ちでない方:<a className={styles.a} onClick={toggleBool}>signup now</a></div>
+                </>}
 
 
 
-          </div>
-        </form>
-
-        {/* <LoginButton type="button" onClick={signInWithGithub}>ログイン</LoginButton>
+            </div>
+          </form>
+          {/* <LoginButton type="button" onClick={signInWithGithub}>ログイン</LoginButton>
     <LoginButton type="button" onClick={logout}>ログアウト</LoginButton> */}
-      </Box>
+        </div>
+      </div>
     </div>
   </>)
 }
